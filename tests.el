c0032f2e9a3ef01237388cd77a32650c82d8399b
Joon Ro <devel.jro@outlook.com>
2019-01-12 16:59:18 -0600

    Move tests.el to the root

--- tests.el
+++ tests.el
@@ -1,4 +1,4 @@
-(load-file "../Assets/.emacs.d/init.el")
+(load-file "./Assets/.emacs.d/init.el")
 
 (require 'org)
 (require 'ox-latex)
@@ -6,14 +6,14 @@
 (setq org-use-property-inheritance t)
 
 ;; Test exporting syllabus
-(with-current-buffer (find-file-noselect "../Courses/Template/Semester Template/Semester Template.org")
+(with-current-buffer (find-file-noselect "./Courses/Template/Semester Template/Semester Template.org")
   (progn
     (re-search-forward ":EXPORT_FILE_NAME: ./Syllabus/Syllabus \(Section 1\)")
     (org-latex-export-to-pdf nil t)
     ))
 
 ;; Test exporting lecture
-(with-current-buffer (find-file-noselect "../Courses/Template/Semester Template/Semester Template.org")
+(with-current-buffer (find-file-noselect "./Courses/Template/Semester Template/Semester Template.org")
   (progn
     (re-search-forward ":EXPORT_FILE_NAME: ./Lectures/01 Course Introduction")
     (org-reveal-export-to-html nil t)
@@ -21,7 +21,7 @@
     ))
 
 ;; Test exporting exam
-(with-current-buffer (find-file-noselect "../Courses/Template/Semester Template/Semester Template.org")
+(with-current-buffer (find-file-noselect "./Courses/Template/Semester Template/Semester Template.org")
   (progn
     (re-search-forward ":EXPORT_FILE_NAME: ./Exams/Exam_1_01_A")
     (org-reveal-export-to-html nil t)
@@ -29,7 +29,7 @@
     ))
 
 ;; Test exporting assignment
-(with-current-buffer (find-file-noselect "../Courses/Template/Semester Template/Semester Template.org")
+(with-current-buffer (find-file-noselect "./Courses/Template/Semester Template/Semester Template.org")
   (progn
     (re-search-forward ":EXPORT_FILE_NAME: ./Assignments/Assignment_1")
     (org-reveal-export-to-html nil t)
