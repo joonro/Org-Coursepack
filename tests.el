f699da6b37e94dd1dc83ef258298305bb531a57e
Joon Ro <devel.jro@outlook.com>
2019-01-12 22:00:37 -0600

    Add tests back

--- tests.el
+++ tests.el
@@ -1,8 +1,40 @@
 (load-file "./Assets/.emacs.d/init.el")
 
 (require 'org)
-;; (require 'ox-latex)
+(require 'ox-latex)
 
 (setq org-use-property-inheritance t)
 
+;; Test exporting syllabus
+(with-current-buffer (find-file-noselect "./Courses/Template/Semester Template/Semester Template.org")
+  (progn
+    (re-search-forward ":EXPORT_FILE_NAME: ./Syllabus/Syllabus \(Section 1\)")
+    ;;(org-latex-export-to-pdf nil t)
+    (org-html-export-to-html nil t)
+    ))
+
+;; Test exporting lecture
+(with-current-buffer (find-file-noselect "./Courses/Template/Semester Template/Semester Template.org")
+  (progn
+    (re-search-forward ":EXPORT_FILE_NAME: ./Lectures/01 Course Introduction")
+    (org-reveal-export-to-html nil t)
+    ;;(org-latex-export-to-pdf nil t)
+    (org-html-export-to-html nil t)
+    ))
+
+;; Test exporting exam
+(with-current-buffer (find-file-noselect "./Courses/Template/Semester Template/Semester Template.org")
+  (progn
+    (re-search-forward ":EXPORT_FILE_NAME: ./Exams/Exam_1_01_A")
+    ;;(org-latex-export-to-pdf nil t)
+    (org-html-export-to-html nil t)
+    ))
+
+;; Test exporting assignment
+(with-current-buffer (find-file-noselect "./Courses/Template/Semester Template/Semester Template.org")
+  (progn
+    (re-search-forward ":EXPORT_FILE_NAME: ./Assignments/Assignment_1")
+    ;;(org-latex-export-to-pdf nil t)
+    (org-html-export-to-html nil t)
+    ))
 
