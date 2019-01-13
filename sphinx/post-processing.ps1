2b3af1a4354ccacca8544ab896e9771c51d76f76
Joon Ro <devel.jro@outlook.com>
2019-01-13 16:53:11 -0600

    Print out status

--- sphinx/post-processing.ps1
+++ sphinx/post-processing.ps1
@@ -7,5 +7,10 @@ Foreach-Object {
     (Get-Content $_.FullName) -Replace '<span class="pre">\\\*\\\*\\\*</span>', '<span class="pre">***</span>' -join "`n"  | Set-Content $_.FullName -NoNewline -Force
 }
 
+echo "Image paths and escape characters for * are fixed."
+
 cp ../Assets/Images/Org-Teaching/*.png ../html/Assets/Images/Org-Teaching/
+
+echo "Images copied to ../html/Assets/Images."
+
 
